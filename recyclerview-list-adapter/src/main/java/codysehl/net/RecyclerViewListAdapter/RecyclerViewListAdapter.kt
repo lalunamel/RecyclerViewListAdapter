d3be9dab9a87257878c1a267412eccc0ed5f50f3
Cody Sehl <cody.sehl@gmail.com>
2018-01-04 17:23:10 -0700

    init. Shamelessly lifted repo template from RxAndroid

---
+++ recyclerview-list-adapter/src/main/java/codysehl/net/RecyclerViewListAdapter/RecyclerViewListAdapter.kt
@@ -0,0 +1,29 @@
+package codysehl.net.RecyclerViewListAdapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+
+abstract class RecyclerViewListAdapterOrig<V: View, I>(var items: List<I> = listOf()): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    companion object {
+        fun <V: View, I>create(viewConstructor: () -> V, update: (V, I) -> Unit): RecyclerViewListAdapterOrig<V, I> {
+            return object: RecyclerViewListAdapterOrig<V, I>() {
+
+                override fun getItemCount(): Int = items.size
+
+                override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+                    val view: V = viewConstructor()
+                    return object: RecyclerView.ViewHolder(view) {}
+                }
+
+                override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                    val item = items[position]
+                    @Suppress("UNCHECKED_CAST")
+                    update(holder.itemView as V, item)
+                }
+
+            }
+        }
+    }
+}
